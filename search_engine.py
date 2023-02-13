d28b5e3c3d193b14f88ec2d392f6e3d957bc0e59
adnan2232 <aa2232783@gmail.com>
2023-02-13 23:27:19 +0530

    Half way through

---
+++ search_engine.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import re
+
+class SearchEngine:
+
+    def __init__(self,df_or_path: str|pd.DataFrame, index_col:int=-1, replacement: dict[re.Pattern,str] = dict()) -> None:
+
+        self.df: pd.DataFrame
+        self.replacement = replacement
+
+        if isinstance(df_or_path,str):
+            try:
+                if df_or_path.split(".")[1] == "csv":
+                    self.set_df_csv(df_or_path,index_col=index_col)
+
+                elif df_or_path.split(".")[1] == "parquet":
+                    self.set_df_parquet(df_or_path)
+
+                else:
+                    raise Exception("Looks like your path file extension is not supported")
+
+            except IndexError:
+                raise Exception("File extension not found in your path")
+        
+        elif isinstance(df_or_path,pd.DataFrame):
+            self.set_df(df_or_path)
+        
+        else:
+            raise Exception("Passed df_or_path should be path to data or a dataframe")
+
+    def set_df(self,df: pd.DataFrame) -> None:
+        self.df = df
+
+    def set_df_parquet(self, path: str) -> None:
+
+        self.df = pd.read_parquet(path)
+
+    def set_df_csv(self, path: str, index_col:int=-1) -> None:
+
+        if index_col >-1:
+            self.df = pd.read_csv(path,index_col=index_col)
+        else:
+            self.df = pd.read_csv(path)
+
+    def text_to_set(self,query: str, replacement: dict[re.Pattern,str] ={}) -> set[str]:
+
+        if not replacement:
+            replacement = self.replacement
+
+        for key,val in replacement.items():
+
+            query = re.sub(key,val,query)
+
+        return set(query.strip().split(" "))
+
+    def exact_match(self, category: str, bag_of_words: list|set|tuple, df: pd.DataFrame = pd.DataFrame()) -> pd.DataFrame:
+        
+        assert hasattr(self,"df") or not df.empty, "No data to query on please set data frame using set_df_parquet, set_df_csv or set_df or pass data frame"
+
+        if df.empty:
+            df = self.df
+
+        ind = df[category].isin(bag_of_words)
+
+        if ind.any():
+            return df.loc[ind].copy()
+
+        return df.copy()
+
+
+
+    
